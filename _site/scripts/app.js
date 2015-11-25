037c4466e86c91169bf1e6a4d3a154ee0ab4cfeb
rmtrigo <rmontrigo@gmail.com>
2015-11-24 17:36:24 -0800

    finished stage 3

--- _site/scripts/app.js
+++
@@ -1,7 +0,0 @@
-angular.module("todoListApp", [])
-	.controller('mainCtrl', function($scope) {
-		$scope.helloWorld = function() {
-			console.log("Hello there! This is the helloWorld " + 
-				"controller function in the main ctrl!");
-		}
-	});
